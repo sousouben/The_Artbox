a940bf931ac7df0dc1317d832078718cd2fdb26f
SOUSOUBENH <75803460+sousouben@users.noreply.github.com>
2023-07-25 18:50:00 +0200

    creation du fichier oeuvre /affichage des oeuvres

--- index.php
+++ index.php
@@ -2,129 +2,41 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="img/favicon-32x32.png" type="image/x-icon">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
     <title>The ArtBox</title>
 </head>
 <body>
-<?php 
-    include('header.php');
-    ?>
+    <!-- Inclusion du contenu du header -->
+    <?php include('header.php'); ?>
+
+    <!-- Inclusion des données des œuvres -->
+    <?php include('oeuvres.php'); ?>
+
     <main>
-        <?php
-        include_once('oeuvres.php')
-        ?>
+        <!-- Section de la liste des œuvres -->
         <div id="liste-oeuvres">
+        <?php foreach ($oeuvres as $oeuvre): ?>
+            <!-- Début de la balise "article" pour chaque œuvre -->
             <article class="oeuvre">
-                <a href="oeuvre-1.php">
-                    <img src="img/clark-van-der-beken.png" alt="Dodomu">
-                    <h2>Dodomu</h2>
-                    <p class="description">Mia Tozerski</p>
+                <!-- Lien vers la page de détails de l'œuvre en utilisant l'identifiant comme paramètre GET -->
+                <a href="oeuvre.php?id=<?php echo $oeuvre['id']; ?>">
+                    <!-- Affichage de l'image de l'œuvre -->
+                    <img src="<?php echo $oeuvre['picture']; ?>" alt="<?php echo $oeuvre['alt'];?>">
+                    <!-- Affichage du nom de l'artiste -->
+                    <h2> <?php echo $oeuvre['artist']; ?> </h2>
+                    <!-- Affichage de la description de l'œuvre -->
+                    <p class="description"> <?php echo $oeuvre['description']; ?> </p>
                 </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-2.php">
-                    <img src="img/pawel-czerwinski-3.png" alt="Aashaaheen Baadal">
-                    <h2>Aashaaheen Baadal</h2>
-                    <p class="description">Anaisha Devi</p>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-3.php">
-                    <img src="img/dan-cristian-padure.png" alt="Nightlife Traffic">
-                    <h2>Nightlife Traffic</h2>
-                    <p class="description">Andrew Forsythe</p>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-4.php">
-                    <img src="img/steve-johnson-5.png" alt="Le refuge de l'Havre">
-                    <h2>Le refuge de l'Havre</h2>
-                    <p class="description">Simon Pelletier</p>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-5.php">
-                    <img src="img/steve-johnson.png" alt="Red Washover">
-                    <h2>Red Washover</h2>
-                    <p class="description">Kit Van Der Borght</p>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-6.php">
-                    <img src="img/pawel-czerwinski.png" alt="Chromatics">
-                    <h2>Chromatics</h2>
-                    <p class="description">Jean-Michel Delatronchette</p>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-7.php">
-                    <img src="img/jazmin-quaynor.png" alt="Digital Negative">
-                    <h2>Digital Negative</h2>
-                    <p class="description">Hamish McKee</p>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-8.php">
-                    <img src="img/steve-johnson-6.png" alt="Blast from the past">
-                    <h2>Blast from the past</h2>
-                    <p class="description">Juliette Baskerville</p>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-9.php">
-                    <img src="img/victor-grabarczyk.png" alt="Hurricane">
-                    <h2>Hurricane</h2>
-                    <p class="description">Natalie Wellington</p>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-10.php">
-                    <img src="img/pawel-czerwinski-2.png" alt="La marée rouge">
-                    <h2>La marée rouge</h2>
-                    <p class="description">Martin Rodriguez</p>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-11.php">
-                    <img src="img/steve-johnson-2.png" alt="Asimilacion">
-                    <h2>Asimilacion</h2>
-                    <p class="description">Angel Sanchez-Fernandez</p>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-12.php">
-                    <img src="img/fly-d.png" alt="La Galaxia Gialla">
-                    <h2>La Galaxia Gialla</h2>
-                    <p class="description">Eduardo Tancredi</p>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-13.php">
-                    <img src="img/orfeas-green.png" alt="Puffy Amalgamate">
-                    <h2>Puffy Amalgamate</h2>
-                    <p class="description">Sandro De Blasi</p>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-14.php">
-                    <img src="img/steve-johnson-4.png" alt="Mirage">
-                    <h2>Mirage</h2>
-                    <p class="description">Stéphanie Kaiser</p>
-                </a>
-            </article>
-            <article class="oeuvre">
-                <a href="oeuvre-15.php">
-                    <img src="img/steve-johnson-3.png" alt="Blaue Gelbe Muster">
-                    <h2>Blaue Gelbe Muster</h2>
-                    <p class="description">Adelheid Von Schreiber</p>
-                </a>
-            </article>
+            </article> 
+            <!-- Fin de la balise "article" pour chaque œuvre -->
+        <?php endforeach; ?>           
         </div>
     </main>
+
+    <!-- Inclusion du contenu du footer -->
     <?php include('footer.php'); ?>
 </body>
 </html>
